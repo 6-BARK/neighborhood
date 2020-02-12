827ec80dda0c4f4bedacf436eef518cbc375a47d
Andrew Song <andrew.song.1122@gmail.com>
2020-02-12 10:25:18 -0800

    write CRUD operations

--- database/index.js
+++ database/index.js
@@ -4,30 +4,25 @@ const fakedata = require ('./fakedata.js')
 const data = fakedata.data;
 
 //connnect to mongodb and create a db called 7-xillow
-mongoose.connect('mongodb://localhost/7-xillow', {useMongoClient: true});
+mongoose.connect('mongodb://localhost/7-xillow', {useMongoClient: true}); // CHANGE THIS YOU SILLY CHILD
 mongoose.connection.dropDatabase(); //drop if the db exists
 let listingSchema = mongoose.Schema({ //create a Schema for the db
-
-neighborhood: Number,
-mapImage: String,
-walk_score: Number,
-transit_score: Number,
-price:Number,
-sqft: Number,
-bedNumber: Number,
-bathNumner: Number,
-address: String,
-nearbyImage:[{type: String}]
-
+  neighborhood: Number,
+  mapImage: String,
+  walk_score: Number,
+  transit_score: Number,
+  price:Number,
+  sqft: Number,
+  bedNumber: Number,
+  bathNumner: Number,
+  address: String,
+  nearbyImage:[{type: String}]
 });
 
-let Houses = mongoose.model('Houses', listingSchema)
+let Houses = mongoose.model('Houses', listingSchema);
 
 let save = (listing)=> {
-
-
-    var newListing = Houses ({
-
+  var newListing = Houses ({
     neighborhood: listing.neighborhood,
     mapImage: listing.mapImage,
     walk_score: listing.walk_score,
@@ -37,36 +32,55 @@ let save = (listing)=> {
     bedNumber: listing.bedNumber,
     bathNumner: listing.bathNumner,
     address: listing.address,
-    nearbyImage: listing.nearbyImage
-    
-    })
-  
+    nearbyImage: listing.nearbyImage    
+  });
 
-    newListing.save((err, newListing) => {
-        if(err) {
-            console.log(err)
-        } else {
-            console.log('this was success')
-        }
-    })
+  newListing.save((err) => {
+      if(err) {
+          console.log(err);
+      } else {
+          console.log(`created listing at ${listing.address}!`);
+      }
+  });
 }
 
 //seed mock data
-var neighborhoodInfo = data(100)
+var neighborhoodInfo = data(100);
 neighborhoodInfo.forEach(listing => save(listing));
 
 //to fetech data from the db
 let fetch = (callback) => {
     //get something from the database ...
   Houses.find((err, documents) => {
-      if(err) {
-        callback (err)
-      } else {
-        callback( null, documents)
-      }
-    })
-
+    if(err) {
+      callback (err)
+    } else {
+      callback( null, documents)
     }
+  });
+}
+
+let update = (listing) => {
+  Houses.replaceOne({address: listing.address}, listing, {upsert: true}, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(`listing at ${listing.address} updated!`);
+    }
+  });
+}
+
+let remove = (listing) => {
+  Houses.findOneAndDelete(listing, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(`listing at ${listing.address} deleted!`);
+    }
+  });
+}
 
 module.exports.save = save;
 module.exports.fetch = fetch;
+module.exports.update = update;
+module.exports.remove = remove;
--- server/index.js
+++ server/index.js
@@ -4,13 +4,22 @@ const path = require('path');
 const app = express();
 // Set a constant for the port that our express server will listen on
 const PORT = 3001;
-const database = require ('../database/index.js')
+const database = require('../database/index.js');
 
-app.use(express.json())
+app.use(express.json());
 
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+// Create / POST - create a new item
+// Read / GET - read an item
+// Update / PUT - update an item
+// Delete / DELETE - delete an item
+
+// add item to db
+app.post('/save', function(req, res) {
+  database.save(req.body);
+});
 
 //get request to get the data from the db
 app.get('/listings', function (req, res) {
@@ -23,6 +32,14 @@ app.get('/listings', function (req, res) {
       res.send(results)
     }
   });
-  });
+});
+
+app.put('/update', function(req, res) {
+  database.update(req.body);
+});
+
+app.delete('/delist', function(req, res) {
+  database.remove(req.body);
+});
 
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
