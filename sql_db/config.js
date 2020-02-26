521c7379f56cc94ff0587b96c3c669cae7b8b1aa
Andrew Song <andrew.song.1122@gmail.com>
2020-02-26 14:58:22 -0800

    changed mysql config

--- sql_db/config.js
+++ sql_db/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    host: '13.52.55.232',
+    // host: '13.52.55.232',
+    socketPath: '/var/run/mysqld/mysqld.sock',
     user: 'xillow',
     password: 'neighborhood',
     database: 'listings'
