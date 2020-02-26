757227ca5bdf1d2e4d03de9c7876950443c8c53b
Andrew Song <andrew.song.1122@gmail.com>
2020-02-26 14:34:25 -0800

    changed mysql config

--- sql_db/config.js
+++ sql_db/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: '13.52.55.232',
-    user: 'killow',
+    host: 'ec2-13-52-55-232.us-west-1.compute.amazonaws.com',
+    user: 'xillow',
     password: 'neighborhood',
     database: 'listings'
   };
