0b9b7efc2104516673b69a08641b72ac8b1b2903
Andrew Song <andrew.song.1122@gmail.com>
2020-02-25 16:09:43 -0800

    changed port

--- testget.js
+++ testget.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function() {
-  http.get(`http://50.18.212.28:3000/api/listings/${Math.ceil(Math.random() * 10000000)}`);
+  http.get(`http://50.18.212.28:3003/api/listings/${Math.ceil(Math.random() * 10000000)}`);
   sleep(1);
 }
