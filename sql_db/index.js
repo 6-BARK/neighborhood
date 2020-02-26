adfcf1ae13aad2a2cba6d074491627447891f336
Andrew Song <andrew.song.1122@gmail.com>
2020-02-25 16:47:17 -0800

    idek

--- sql_db/index.js
+++ sql_db/index.js
@@ -7,7 +7,7 @@ const getHouseInfo = (houseId, callback) => {
     // connection.query(`SELECT * FROM houses WHERE id=${houseId}`, (err, listingInfo) => {
     connection.query(`SELECT * FROM houses FORCE KEY (PRIMARY) WHERE id=${houseId}`, (err, listingInfo) => {
         if(err) {
-            console.log('ur mum gey');
+            console.log('err');
             callback(err);
         } else {
             callback(null, listingInfo);
